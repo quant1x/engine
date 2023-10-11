9747f0f413e6305e19710095710dfbf8513a951c
wangfeng <wangfengxy@sina.cn>
2023-10-12 07:06:42 +0800

    变更源文件名

--- command/command_app.go
+++ command/command_app.go
@@ -6,6 +6,11 @@ var (
 	barIndex    = 1
 )
 
+// UpdateApplicationName 更新应用(Application)名称
+func UpdateApplicationName(name string) {
+	Application = name
+}
+
 // UpdateApplicationVersion 更新版本号
 func UpdateApplicationVersion(v string) {
 	MinVersion = v

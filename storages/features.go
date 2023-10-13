0ed7217cf72f0ca4d7163d2cf3ed2c8125b409e6
wangfeng <wangfengxy@sina.cn>
2023-10-13 09:40:05 +0800

    测试协程方式跑特征数据

--- storages/features.go
+++ storages/features.go
@@ -6,6 +6,7 @@ import (
 	"gitee.com/quant1x/engine/factors"
 	"gitee.com/quant1x/engine/market"
 	"gitee.com/quant1x/engine/smart"
+	"gitee.com/quant1x/gox/fastqueue"
 	"gitee.com/quant1x/gox/progressbar"
 	"gitee.com/quant1x/gox/text/runewidth"
 	"gitee.com/quant1x/gox/util/treemap"
@@ -75,6 +76,62 @@ func updateOneFeature(parent, bar *progressbar.Bar, adapter cachel5.CacheAdapter
 	parent.Add(1)
 }
 
+const (
+	queueMax = 4096
+)
+
+type featureTask struct {
+	wg           *sync.WaitGroup
+	bar          *progressbar.Bar
+	data         factors.Feature
+	securityCode string
+	cacheDate    string
+	featureDate  string
+}
+
+// 更新单个特征
+func v2updateOneFeature(parent, bar *progressbar.Bar, adapter cachel5.CacheAdapter, cacheDate, featureDate string, op cache.OpKind, barIndex *int) {
+	mapFeature := treemap.NewWithStringComparator()
+	var wg sync.WaitGroup
+	dataSource := adapter.Factory(featureDate, "")
+	_ = dataSource.Init(barIndex, featureDate)
+	allCodes := market.GetCodeList()
+	queue := fastqueue.NewQueue[featureTask](queueMax)
+	queue.SetReadEvent(func(data []featureTask) {
+		for _, v := range data {
+			//v.wg.Add(1)
+			updateStockFeature(v.wg, v.bar, v.data, v.securityCode, v.cacheDate, v.featureDate, op, mapFeature)
+			//fmt.Println(v)
+			//bar.Add(1)
+		}
+	})
+	for _, code := range allCodes {
+		data := adapter.Factory(cacheDate, code).(factors.Feature)
+		if data.Kind() != factors.FeatureHistory {
+			history := smart.GetL5History(code, cacheDate)
+			if history != nil {
+				data = data.FromHistory(*history)
+			}
+		}
+		wg.Add(1)
+		go queue.Push(featureTask{
+			wg:           &wg,
+			bar:          bar,
+			data:         data,
+			securityCode: code,
+			cacheDate:    cacheDate,
+			featureDate:  featureDate,
+		})
+	}
+	queue.Wait()
+	// 加载缓存
+	adapter.Checkout(cacheDate)
+	// 合并
+	adapter.Merge(mapFeature)
+	//wg.Wait()
+	parent.Add(1)
+}
+
 // FeaturesUpdate 更新特征
 func FeaturesUpdate(barIndex *int, cacheDate, featureDate string, plugins []cache.DataPlugin, op cache.OpKind) {
 	moduleName := "特征数据"
@@ -96,66 +153,16 @@ func FeaturesUpdate(barIndex *int, cacheDate, featureDate string, plugins []cach
 			}
 		}
 	}
-	//var wg sync.WaitGroup
+	var topWaitGroup sync.WaitGroup
 	allCodes := market.GetCodeList()
 	cacheCount := len(cacheList)
 	barCache := progressbar.NewBar(*barIndex, "执行["+moduleName+"]", cacheCount)
 	for _, adapter := range cacheList {
+		topWaitGroup.Add(1)
 		codeCount := len(allCodes)
 		barCode := progressbar.NewBar(*barIndex+1, "执行["+adapter.Name()+"]", codeCount)
-		//wg.Add(1)
-		//updateOneFeature(&wg, barCache, barCode, adapter, cacheDate, featureDate, op, barIndex)
 		updateOneFeature(barCache, barCode, adapter, cacheDate, featureDate, op, barIndex)
+		topWaitGroup.Done()
 	}
-	//wg.Wait()
+	topWaitGroup.Wait()
 }
-
-//// RepairFeatures 修复特征数据
-//func RepairFeatures(barIndex *int, cacheDate, featureDate string) {
-//	moduleName := "修复特征数据" + cacheDate
-//	// 1. 获取全部注册的数据集插件
-//	mask := cache.PluginMaskFeature
-//	//dataSetList := flash.DataSetList()
-//	plugins := cache.Plugins(mask)
-//	var cacheList []cachel5.CacheAdapter
-//	maxWidth := 0
-//	for _, plugin := range plugins {
-//		adapter, ok := plugin.(cachel5.CacheAdapter)
-//		if ok {
-//			cacheList = append(cacheList, adapter)
-//			width := runewidth.StringWidth(adapter.Name())
-//			if width > maxWidth {
-//				maxWidth = width
-//			}
-//		}
-//	}
-//	allCodes := market.GetCodeList()
-//	cacheCount := len(cacheList)
-//	barCache := progressbar.NewBar(*barIndex, "执行["+moduleName+"]", cacheCount)
-//	for _, adapter := range cacheList {
-//		mapFeature := treemap.NewWithStringComparator()
-//		codeCount := len(allCodes)
-//		barCode := progressbar.NewBar(*barIndex+1, "执行["+adapter.Name()+"]", codeCount)
-//		dataSource := adapter.Factory(featureDate, "")
-//		_ = dataSource.Init(barIndex, featureDate)
-//		for _, code := range allCodes {
-//			data := adapter.Factory(cacheDate, code).(factors.Feature)
-//			if data.Kind() != factors.FeatureHistory {
-//				history := smart.GetL5History(code, cacheDate)
-//				if history != nil {
-//					data = data.FromHistory(*history)
-//				}
-//			}
-//			data.Repair(code, cacheDate, featureDate, true)
-//			mapFeature.Put(code, data)
-//			barCode.Add(1)
-//		}
-//		// 加载缓存
-//		adapter.Checkout(cacheDate)
-//		// 合并
-//		adapter.Merge(mapFeature)
-//		_ = adapter
-//		barCache.Add(1)
-//	}
-//	_ = allCodes
-//}

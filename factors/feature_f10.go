aa443c904411cf30861f3bdb7ddb35ff18e86730
wangfeng <wangfengxy@sina.cn>
2023-10-16 08:43:05 +0800

    调整F10的csv字段名

--- factors/feature_f10.go
+++ factors/feature_f10.go
@@ -17,29 +17,29 @@ const (
 
 // F10 证券基本面
 type F10 struct {
-	Date           string  `name:"日期"`           // 日期
-	Code           string  `name:"代码"`           // 代码
-	Name           string  `name:"名称"`           // 名称
-	SubNew         bool    `name:"次新股"`          // 是否次新股
-	VolUnit        int     `name:"每手"`           // 每手单位
-	DecimalPoint   int     `name:"小数点"`          // 小数点
-	IpoDate        string  `name:"上市日期"`         // 上市日期
-	UpdateDate     string  `name:"更新日期"`         // 更新日期
-	TotalCapital   float64 `name:"总股本"`          // 总股本
-	Capital        float64 `name:"流通股本"`         // 流通股本
-	FreeCapital    float64 `name:"自由流通股本"`       // 自由流通股本
-	Top10Capital   float64 `name:"前十大流通股东总股本"`   // 前十大流通股东股本
-	Top10Change    float64 `name:"前十大流通股东总股本变化"` //前十大流通股东股本变化
-	ChangeCapital  float64 `name:"前十大流通股东持仓变化"`  // 前十大流通股东持仓变化
-	IncreaseRatio  float64 `name:"当期增持比例"`       // 当期增持比例
-	ReductionRatio float64 `name:"当期减持比例"`       // 当期减持比例
-	BPS            float64 `name:"每股净资产"`        // 每股净资产
-	BasicEPS       float64 `name:"每股收益"`         // 每股收益
-	SafetyScore    int     `name:"安全分"`          // 通达信安全分
-	Increases      int     `name:"增持"`           // 公告-增持
-	Reduces        int     `name:"减持"`           // 公告-减持
-	Risk           int     `name:"风险数"`          // 公告-风险数
-	RiskKeywords   string  `name:"风险关键词"`        // 公告-风险关键词
+	Date           string  `name:"日期" dataframe:"date"`                    // 日期
+	Code           string  `name:"代码" dataframe:"code"`                    // 代码
+	Name           string  `name:"名称" dataframe:"name"`                    // 名称
+	SubNew         bool    `name:"次新股" dataframe:"sub_new"`                // 是否次新股
+	VolUnit        int     `name:"每手" dataframe:"vol_unit"`                // 每手单位
+	DecimalPoint   int     `name:"小数点" dataframe:"decimal_point"`          // 小数点
+	IpoDate        string  `name:"上市日期" dataframe:"ipo_date"`              // 上市日期
+	UpdateDate     string  `name:"更新日期" dataframe:"update_date"`           // 更新日期
+	TotalCapital   float64 `name:"总股本" dataframe:"total_capital"`          // 总股本
+	Capital        float64 `name:"流通股本" dataframe:"capital"`               // 流通股本
+	FreeCapital    float64 `name:"自由流通股本" dataframe:"free_capital"`        // 自由流通股本
+	Top10Capital   float64 `name:"前十大流通股东总股本" dataframe:"top_10_capital"`  // 前十大流通股东股本
+	Top10Change    float64 `name:"前十大流通股东总股本变化" dataframe:"top_10_change"` //前十大流通股东股本变化
+	ChangeCapital  float64 `name:"前十大流通股东持仓变化" dataframe:"change_capital"` // 前十大流通股东持仓变化
+	IncreaseRatio  float64 `name:"当期增持比例" dataframe:"increase_ratio"`      // 当期增持比例
+	ReductionRatio float64 `name:"当期减持比例" dataframe:"reduction_ratio"`     // 当期减持比例
+	BPS            float64 `name:"每股净资产" dataframe:"bps"`                  // 每股净资产
+	BasicEPS       float64 `name:"每股收益" dataframe:"basic_eps"`             // 每股收益
+	SafetyScore    int     `name:"安全分" dataframe:"safety_score"`           // 通达信安全分
+	Increases      int     `name:"增持" dataframe:"increases"`               // 公告-增持
+	Reduces        int     `name:"减持" dataframe:"reduces"`                 // 公告-减持
+	Risk           int     `name:"风险数" dataframe:"risk"`                   // 公告-风险数
+	RiskKeywords   string  `name:"风险关键词" dataframe:"risk_keywords"`        // 公告-风险关键词
 }
 
 func (f *F10) Check(cacheDate, featureDate string) {

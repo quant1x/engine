51c7aeb4000c91dba90e54d9b0950e7def1c2747
wangfeng <wangfengxy@sina.cn>
2023-10-17 13:25:30 +0800

    收敛cache1d的缓存文件路径函数

--- cachel5/adapter.go
+++ cachel5/adapter.go
@@ -1,10 +1,33 @@
 package cachel5
 
 import (
+	"fmt"
+	"gitee.com/quant1x/engine/cache"
 	"gitee.com/quant1x/engine/factors"
 	"gitee.com/quant1x/gox/util/treemap"
+	"strings"
 	"sync"
 )
+
+const (
+	// 闪存路径
+	cache1dPrefix = "flash"
+	// 默认证券代码为上证指数
+	defaultSecurityCode = "sh000001"
+)
+
+// 返回文件路径, 指定关键字和日期
+func getCache1DFilepath(key, date string) string {
+	cachePath, key, found := strings.Cut(key, "/")
+	if !found {
+		key = cachePath
+		cachePath = cache1dPrefix
+	}
+	cachePath = cache.GetRootPath() + "/" + cachePath
+	year := date[:4]
+	filename := fmt.Sprintf("%s/%s/%s.%s", cachePath, year, key, date)
+	return filename
+}
 
 // CacheAdapter 缓存适配器
 //

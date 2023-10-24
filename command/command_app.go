a3d388fe673d0913fe06fc5d48d4ea0ff07b2acf
wangfeng <wangfengxy@sina.cn>
2023-10-24 10:50:07 +0800

    增加修订application的初始化代码

--- command/command_app.go
+++ command/command_app.go
@@ -1,10 +1,21 @@
 package command
 
+import (
+	"os"
+	"path/filepath"
+)
+
 var (
 	Application = "stock"
 	MinVersion  = "0.0.1" // 版本号
 	barIndex    = 1
 )
+
+func init() {
+	path, _ := os.Executable()
+	_, exec := filepath.Split(path)
+	Application = exec
+}
 
 // UpdateApplicationName 更新应用(Application)名称
 func UpdateApplicationName(name string) {

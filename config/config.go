15ef7b157569a2eb566eac09a672ca413ed5b357
wangfeng <wangfengxy@sina.cn>
2023-12-11 08:39:28 +0800

    调整定时任务的配置方式

--- config/config.go
+++ config/config.go
@@ -46,8 +46,8 @@ type Quant1XConfig struct {
 
 // RuntimeParameter 运行时配置参数
 type RuntimeParameter struct {
-	Pprof   PprofParameter `yaml:"pprof"`
-	Crontab map[string]Job `yaml:"crontab"`
+	Pprof   PprofParameter          `yaml:"pprof"`
+	Crontab map[string]JobParameter `yaml:"crontab"`
 }
 
 // GetConfigFilename 获取配置文件路径
--- config/config_crontab.go
+++ config/config_crontab.go
@@ -1,13 +1,26 @@
 package config
 
-// Job 定时任务配置
-type Job struct {
-	Name    string `yaml:"name" default:""`     // 任务名称
-	Trigger string `yaml:"trigger"  default:""` // 触发条件
-	Enable  bool   `yaml:"enable" default:"true"`
+// JobParameter 定时任务配置
+type JobParameter struct {
+	Name    string `yaml:"name" default:""`       // 任务名称
+	Trigger string `yaml:"trigger"  default:""`   // 触发条件
+	Enable  bool   `yaml:"enable" default:"true"` // 任务是否有效
 }
 
-// JobConfig 获取定时任务配置
-func JobConfig() map[string]Job {
+// CrontabConfig 获取定时任务配置
+func CrontabConfig() map[string]JobParameter {
 	return GlobalConfig.Runtime.Crontab
+}
+
+// GetJobParameter 获取计划执行任务
+func GetJobParameter(name string) *JobParameter {
+	mapJob := CrontabConfig()
+	if len(mapJob) == 0 {
+		return nil
+	}
+	v, ok := mapJob[name]
+	if ok {
+		return &v
+	}
+	return nil
 }

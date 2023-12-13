cb73fc376b2cb82a7aead90633e656b796881b47
wangfeng <wangfengxy@sina.cn>
2023-12-14 04:25:55 +0800

    新增通过策略编码获取用于qmt系统的string类型的策略名, 大写S作为前缀后面跟quant1x系统的策略编码

--- models/stategy.go
+++ models/stategy.go
@@ -67,7 +67,12 @@ type Strategy interface {
 // QmtStrategyName 获取用于QMT系统的策略名称
 func QmtStrategyName(model Strategy) string {
 	id := model.Code()
-	return fmt.Sprintf("S%d", id)
+	return QmtStrategyNameFromId(id)
+}
+
+// QmtStrategyNameFromId 通过策略ID返回用于在QMT系统中表示的string类型的策略名称
+func QmtStrategyNameFromId(strategyCode int) string {
+	return fmt.Sprintf("S%d", strategyCode)
 }
 
 // QmtOrderRemark 获取用于QMT系统的订单备注

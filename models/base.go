d388232db451f51e2b562d5e3519622c5c0bcff0
wangfeng <wangfengxy@sina.cn>
2023-12-14 11:41:20 +0800

    合并部分小功能代码

--- models/base.go
+++ models/base.go
@@ -1,6 +1,18 @@
 package models
 
+import "gitee.com/quant1x/engine/config"
+
 const (
 	// CACHE_STRATEGY_PATH 策略文件存储路径
 	CACHE_STRATEGY_PATH = "strategy"
 )
+
+var (
+	CountDays int // 统计多少天, 由控制台传入数值
+	CountTopN int // 统计前N, 由控制台传入数值
+)
+
+// AllStockTopN 最大输出多少只个股
+func AllStockTopN() int {
+	return config.OrderConfig().TopN
+}

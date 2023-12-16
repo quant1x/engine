2a4fa101ba47af3a65674b564ab010f5723cbf7e
wangfeng <wangfengxy@sina.cn>
2023-12-16 10:06:03 +0800

    增加交易时段测试代码

--- services/task_sell_test.go
+++ services/task_sell_test.go
@@ -2,10 +2,24 @@ package services
 
 import (
 	"fmt"
+	"gitee.com/quant1x/engine/config"
+	"gitee.com/quant1x/engine/models"
 	"testing"
 )
 
 func TestTaskSell_getEarlierDate(t *testing.T) {
 	v := getEarlierDate(1)
+	fmt.Println(v)
+}
+
+func TestTaskSell_LastSession(t *testing.T) {
+	sellStrategyCode := models.ModelOneSizeFitsAllSells
+	// 1. 获取117号策略(卖出)
+	sellRule := config.GetTradeRule(sellStrategyCode)
+	if sellRule == nil {
+		return
+	}
+	timestamp := "14:55:00"
+	v := sellRule.Session.IsTodayLastSession(timestamp)
 	fmt.Println(v)
 }

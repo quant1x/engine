213a9466a0bdbfa3f2aacfd8476fb5c7786e4348
wangfeng <wangfengxy@sina.cn>
2023-12-27 17:06:11 +0800

    修订测试代码

--- rules/rule_f10_test.go
+++ rules/rule_f10_test.go
@@ -2,6 +2,7 @@ package rules
 
 import (
 	"fmt"
+	"gitee.com/quant1x/engine/config"
 	"gitee.com/quant1x/engine/models"
 	"testing"
 )
@@ -11,10 +12,9 @@ func Test_baseFilter(t *testing.T) {
 	code = "sh600622"
 	code = "sh601188"
 	code = "sz002682"
-	stockShots := models.BatchSnapShot([]string{code})
-	if len(stockShots) > 0 {
-		snapshot := stockShots[0]
-		passed, failKind, err := Filter(snapshot)
-		fmt.Println(passed, failKind, err)
-	}
+	snapshot := models.GetTick(code)
+	strategyParametet := config.GetStrategyParameterByCode(0)
+	passed, failKind, err := Filter(strategyParametet.Rules, *snapshot)
+	fmt.Println(passed, failKind, err)
+
 }

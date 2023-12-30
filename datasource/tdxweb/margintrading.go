652460d4ab66567bdf9bdd0d068130115cb0454a
wangfeng <wangfengxy@sina.cn>
2023-12-30 09:36:23 +0800

    测试通达信问小达的接口, 失败

--- datasource/tdxweb/margintrading.go
+++ datasource/tdxweb/margintrading.go
@@ -7,7 +7,8 @@ import (
 
 const (
 	// https://wenda.tdx.com.cn/site/wenda/stock_index.html?message=%E8%9E%8D%E8%B5%84%E8%9E%8D%E5%88%B8
-	urlMarginTrading = "https://wenda.tdx.com.cn/TQL?Entry=NLPSE.NLPQuery&RI=6C07"
+	//urlMarginTrading = "https://wenda.tdx.com.cn/TQL?Entry=JNLPSE.getAllCode&RI=6C07"
+	urlMarginTrading = "https://wenda.tdx.com.cn/TQL?Entry=JNLPSE.getAllCode&RI=6BFD"
 )
 
 func MarginTrading() {
@@ -20,9 +21,19 @@ func MarginTrading() {
 	//	"remark":    {models.QmtOrderRemark(model)},
 	//}
 	//body := params.Encode()
-	body := `[{"nlpse_id":"7311044342301379250","op_flag":1,"sec_code":"","order_field":"sec_code","dynamic_order":"","order_flag":"1","POS":"0","COUNT":"30","timestamps":0,"RANG":"AG"}]`
+	body := `[{"nlpse_id":"7318110250698020161","op_flag":1,"sec_code":"","order_field":"sec_code","dynamic_order":"","order_flag":"1","POS":"0","COUNT":"30","timestamps":0,"RANG":"AG"}]`
+	//body = `[{"op_flag":1,"sec_code":"","order_field":"sec_code","dynamic_order":"","order_flag":"1","POS":"0","COUNT":"30","timestamps":0,"RANG":"AG"}]`
+	body = `[{"nlpseId":"7318094960614448284","orderField":"chg","orderFlag":"0"}]`
 	//logger.Infof("trader-order: %s", body)
-	data, err := http.Post(urlMarginTrading, body)
+	header := map[string]any{
+		http.ContextType: "application/x-www-form-urlencoded" + "; charset=UTF-8",
+		//"Cookie":         "Hm_lvt_5c4c948b141e4d66943a8430c3d600d0=1703193725; Hm_lpvt_5c4c948b141e4d66943a8430c3d600d0=1703720553; LST=10; ASPSessionID=3755195075085152819",
+		"Cookie":  "Hm_lvt_5c4c948b141e4d66943a8430c3d600d0=1703193725; Hm_lpvt_5c4c948b141e4d66943a8430c3d600d0=1703720553; LST=10; ASPSessionID=3755195113739858633",
+		"Origin":  "https://wenda.tdx.com.cn",
+		"Referer": "https://wenda.tdx.com.cn/",
+	}
+
+	data, _, err := http.Request(urlMarginTrading, http.MethodPost, body, header)
 
 	fmt.Println(data, err)
 }

34ac3b81f778b3fc792dc4490f20b5bc8fdd37aa
wangfeng <wangfengxy@sina.cn>
2024-01-01 06:05:59 +0800

    测试代码: 新增,批量转换证券代码为qmt支持的格式

--- market/market_test.go
+++ market/market_test.go
@@ -2,6 +2,8 @@ package market
 
 import (
 	"fmt"
+	"gitee.com/quant1x/gotdx/proto"
+	"strings"
 	"testing"
 )
 
@@ -15,7 +17,20 @@ func TestGetCodeList(t *testing.T) {
 func TestPriceLimit(t *testing.T) {
 	code := "sh603598"
 	lastClose := 19.00
-
 	up, down := PriceLimit(code, lastClose)
 	fmt.Println(up, down)
+}
+
+func TestGetQmtCodeList(t *testing.T) {
+	batchMax := 1000
+	codes := GetStockCodeList()
+	codes = codes[0:batchMax]
+	var list []string
+	for _, v := range codes {
+		_, mflag, symbol := proto.DetectMarket(v)
+		securityCode := fmt.Sprintf("%s.%s", symbol, strings.ToUpper(mflag))
+		list = append(list, securityCode)
+	}
+	fmt.Println(strings.Join(list, ","))
+	_ = batchMax
 }

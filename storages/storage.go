065341731656f81c4beecb2dc311b35e57fd01d6
wangfeng <wangfengxy@sina.cn>
2024-01-18 11:40:49 +0800

    简化更新股票池功能, 取消单一策略的选股列表的落盘, 统一归到股票池

--- storages/storage.go
+++ storages/storage.go
@@ -9,6 +9,7 @@ import (
 	"gitee.com/quant1x/gox/api"
 	"gitee.com/quant1x/pandas"
 	"os"
+	"path/filepath"
 )
 
 var (
@@ -16,17 +17,27 @@ var (
 )
 
 const (
-	StrategiesPath = "zero-sum" // 策略结果数据文件存储路径
+	StrategiesPath = "quant" // 策略结果数据文件存储路径
 )
 
 // GetResultCachePath 获取结果缓存路径
 func GetResultCachePath() string {
-	path := fmt.Sprintf("%s/%s", cache.GetRootPath(), StrategiesPath)
+	path := filepath.Join(cache.GetRootPath(), StrategiesPath)
 	return path
 }
 
-// OutputStatistics 输出策略结果
-func OutputStatistics(model models.Strategy, date string, v []models.Statistics) {
+func getResultOrderFilename(date string, topN int) string {
+	date = exchange.FixTradeDate(date, cache.FilenameDate)
+	path := GetResultCachePath()
+	name := fmt.Sprintf("%s-%d.csv", date, topN)
+	filename := filepath.Join(path, name)
+	return filename
+}
+
+// fileOrderModelOutputStatistics 文件单输出策略结果
+//
+// Deprecated: 文件单模式已废弃, 推荐使用 OutputStatistics [wangfeng on 2024/1/18 08:39]
+func fileOrderModelOutputStatistics(model models.Strategy, date string, v []models.Statistics) {
 	df := pandas.LoadStructs(v)
 	if df.Nrow() == 0 {
 		return
@@ -39,6 +50,7 @@ func OutputStatistics(model models.Strategy, date string, v []models.Statistics)
 	topN := tradeRule.Total
 	orderFlag := model.OrderFlag()
 	date = exchange.FixTradeDate(date, cache.FilenameDate)
+	// 策略结果文件名
 	filename := fmt.Sprintf("%s/%s-%d.csv", GetResultCachePath(), date, topN)
 	_ = df.WriteCSV(filename)
 	updateTime, _ := api.ParseTime(v[0].UpdateTime)
@@ -59,5 +71,16 @@ func OutputStatistics(model models.Strategy, date string, v []models.Statistics)
 			api.CloseQuietly(file)
 		}
 	}
+	stockPoolMerge(model, date, v, topN)
+}
+
+// OutputStatistics 输出策略结果
+func OutputStatistics(model models.Strategy, date string, v []models.Statistics) {
+	tradeRule := config.GetStrategyParameterByCode(model.Code())
+	if tradeRule == nil || !tradeRule.Enable() || tradeRule.Total == 0 {
+		// 配置不存在, 或者规则无效
+		return
+	}
+	topN := tradeRule.Total
 	stockPoolMerge(model, date, v, topN)
 }

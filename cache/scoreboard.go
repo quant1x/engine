ab46c0db529d45712e5f378f45727cc698c370d3
wangfeng <wangfengxy@sina.cn>
2024-01-19 07:10:07 +0800

    调整记分牌String方法

--- cache/scoreboard.go
+++ cache/scoreboard.go
@@ -38,6 +38,6 @@ func (this *ScoreBoard) Add(delta int, take time.Duration) {
 }
 
 func (this *ScoreBoard) String() string {
-	s := fmt.Sprintf("kind: %d, total: %d, crosstime: %s, max: %d, min: %d, speed: %f", this.Kind, this.Count, this.CrossTime, this.Max, this.Min, this.Speed)
+	s := fmt.Sprintf("name: %s, kind: %d, total: %d, crosstime: %s, max: %d, min: %d, speed: %f", this.Name, this.Kind, this.Count, this.CrossTime, this.Max, this.Min, this.Speed)
 	return s
 }

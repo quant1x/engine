2d2606b858b4ea7a633ec9c426bfebbbf692b791
wangfeng <wangfengxy@sina.cn>
2024-01-28 11:03:43 +0800

    新增实时计算EMA的函数

---
+++ realtime/exponential_moving_average.go
@@ -0,0 +1,26 @@
+package realtime
+
+import (
+	"gitee.com/quant1x/engine/factors"
+	"gitee.com/quant1x/pandas/formula"
+)
+
+// AlphaOfExponentialMovingAverage 计算EMA的alpha值
+func AlphaOfExponentialMovingAverage(period int) float64 {
+	return formula.AlphaOfEMA(period)
+}
+
+// IncrementalExponentialMovingAverage 增量计算 指数移动平均线
+func IncrementalExponentialMovingAverage(now, last, alpha float64) float64 {
+	return formula.EmaIncr(now, last, alpha)
+}
+
+// DynamicExponentialMovingAverage 动态EMA
+//
+//	返回当前值以及最高值和最低值
+func DynamicExponentialMovingAverage(snapshot factors.QuoteSnapshot, last, alpha float64) (ema, emaHigh, emaLow float64) {
+	ema = IncrementalExponentialMovingAverage(snapshot.Price, last, alpha)
+	emaHigh = IncrementalExponentialMovingAverage(snapshot.High, last, alpha)
+	emaLow = IncrementalExponentialMovingAverage(snapshot.Low, last, alpha)
+	return
+}

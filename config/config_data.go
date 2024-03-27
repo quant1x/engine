c2ef208bf8cf452aae2eab294a97acb7dbcee0da
wangfeng <wangfengxy@sina.cn>
2024-03-28 07:45:57 +0800

    特征配置新增波浪(wave)使用的字段及周期数

--- config/config_data.go
+++ config/config_data.go
@@ -6,6 +6,11 @@ type DataParameter struct {
 	Feature FeatureParameter               `name:"feature" yaml:"feature"`
 }
 
+// GetDataConfig 取得数据配置
+func GetDataConfig() DataParameter {
+	return GlobalConfig.Data
+}
+
 // HistoricalTradingDataParameter 历史成交数据参数
 type HistoricalTradingDataParameter struct {
 	BeginDate string `name:"默认开始日期" yaml:"begin_date" default:"2023-10-01"`
@@ -13,10 +18,17 @@ type HistoricalTradingDataParameter struct {
 
 // FeatureParameter 特征参数
 type FeatureParameter struct {
-	Tendency int `yaml:"tendency" default:"0"` // 策略是趋势主导还是股价主导, 默认是0, 0-股价主导,1-趋势主导,2-股价或趋势
+	Tendency int         `yaml:"tendency" default:"0"` // 策略是趋势主导还是股价主导, 默认是0, 0-股价主导,1-趋势主导,2-股价或趋势
+	Wave     FeatureWave `name:"波浪" yaml:"wave"`
 }
 
-// GetDataConfig 取得数据配置
-func GetDataConfig() DataParameter {
-	return GlobalConfig.Data
+// FeatureWave 特征 - 波浪
+type FeatureWave struct {
+	Field   FeatureWaveField `name:"波浪检测字段" yaml:"field"`             // K线检测字段
+	Periods int              `name:"周期数" yaml:"periods" default:"89"` // 波浪检测K线周期数, 默认89天
+}
+
+type FeatureWaveField struct {
+	Peak   string `yaml:"peak" default:"close"`   // K线检测 - 波峰字段, 默认是收盘价
+	Valley string `yaml:"valley" default:"close"` // K线检测 - 波谷字段, 默认是收盘价
 }

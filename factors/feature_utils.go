6769c3834df46d54496ac6a5c8863199e04abda3
wangfeng <wangfengxy@sina.cn>
2024-04-04 05:13:10 +0800

    新增获取股价的小数点位数

---
+++ factors/feature_utils.go
@@ -0,0 +1,26 @@
+package factors
+
+import (
+	"gitee.com/quant1x/engine/cache"
+	"gitee.com/quant1x/gotdx/securities"
+	"time"
+)
+
+// GetTimestamp 时间戳
+//
+//	格式: YYYY-MM-DD hh:mm:ss.SSS
+func GetTimestamp() string {
+	now := time.Now()
+	return now.Format(cache.TimeStampMilli)
+}
+
+// PriceDigits 获取证券标的价格保留小数点后几位
+//
+//	默认范围2, 即小数点后2位
+func PriceDigits(securityCode string) int {
+	securityInfo, ok := securities.CheckoutSecurityInfo(securityCode)
+	if !ok {
+		return 2
+	}
+	return int(securityInfo.DecimalPoint)
+}

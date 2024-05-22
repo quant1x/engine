f85a92f9b5e3a421812dcd412bfe2aa013915852
wangfeng <wangfengxy@sina.cn>
2024-05-23 07:45:32 +0800

    修订测试代码

--- factors/feature_ism_test.go
+++ factors/feature_ism_test.go
@@ -12,9 +12,9 @@ func TestISM(t *testing.T) {
 	code := "000737"
 	date := "2024-03-21"
 	cacheDate, featureDate := cache.CorrectDate(date)
-	s8 := NewInvestmentSentimentMaster(cacheDate, code)
-	s8.Update(code, cacheDate, featureDate, true)
-	data, _ := json.Marshal(s8)
+	ism := NewInvestmentSentimentMaster(cacheDate, code)
+	ism.Update(code, cacheDate, featureDate, true)
+	data, _ := json.Marshal(ism)
 	text := api.Bytes2String(data)
 	fmt.Println(text)
 }

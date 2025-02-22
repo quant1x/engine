58ffc4b629ee37c40bf9592f04331835b451d734
wangfeng <wangfengxy@sina.cn>
2025-02-22 09:04:22 +0800

    调整成交数据的测试代码

--- factors/dataset_trans_test.go
+++ factors/dataset_trans_test.go
@@ -6,10 +6,18 @@ import (
 	"testing"
 )
 
+func TestTransactionOld(t *testing.T) {
+	code := "sh881200"
+	date := "2025-02-21"
+	list := base.GetHistoricalTradingData(code, date)
+	v := CountInflow(list, code, date)
+	fmt.Printf("%+v\n", v)
+}
+
 func TestTransaction(t *testing.T) {
 	code := "sh881200"
-	date := "2024-01-09"
-	list := base.GetHistoricalTradingData(code, date)
+	date := "2025-02-21"
+	list := base.CheckoutTransactionData(code, date, true)
 	v := CountInflow(list, code, date)
 	fmt.Printf("%+v\n", v)
 }

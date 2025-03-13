05f7635b8e2b525bb28b37934985b73ddf74488f
wangfeng <wangfengxy@sina.cn>
2025-03-13 08:10:19 +0800

    明确基础数据接口的日期全部使用特征日期featureDate

--- factors/base_data.go
+++ factors/base_data.go
@@ -10,11 +10,11 @@ import (
 //	检索的规则是按日期和代码进行查询
 type BaseData interface {
 	// Clone 克隆一个BaseData, 是所有写操作的基础
-	Clone(date, code string) BaseData
+	Clone(featureDate, code string) BaseData
 	// Update 更新数据
-	Update(date, code string) error
+	Update(featureDate, code string) error
 	// Repair 回补数据
-	Repair(date, code string) error
+	Repair(featureDate, code string) error
 	// Increase 增量计算, 用快照增量计算特征
 	Increase(snapshot quotes.Snapshot) error
 }
